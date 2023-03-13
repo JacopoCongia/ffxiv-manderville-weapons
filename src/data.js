5d6dfeca5d995da84356278762cd4f30bd30b95f
jacopo.congia <jacopo.congia@gmail.com>
2023-03-13 20:10:08 +0100

    Testing new data format

--- src/data.js
+++ src/data.js
@@ -1,5 +1,5 @@
-const data = [
-    [
+const data = {
+    manderville: [
         {
             id: 0,
             wpnName: "Manderville Sword",
@@ -159,9 +159,9 @@ const data = [
             wpnJobShort: "SGE",
             icon: "/icons/Manderville_Milpreves_Icon.png",
             isSelected: false
-        },
+        }
     ],
-    [
+    amazingManderville: [
         {
             id: 0,
             wpnName: "Amazing Manderville Sword",
@@ -322,7 +322,333 @@ const data = [
             icon: "/icons/Amazing_Manderville_Milpreves_Icon.png",
             isSelected: false
         },
-    ],
-]
+    ]
+}
+// const data = [
+//     [
+//         {
+//             id: 0,
+//             wpnName: "Manderville Sword",
+//             wpnJob: "Paladin",
+//             wpnJobShort: "PLD",
+//             icon: "/icons/Manderville_Sword_Icon.png",
+//             isSelected: false
+//         },
+//         {
+//             id: 1,
+//             wpnName: "Manderville Kite Shield",
+//             wpnJob: "Paladin",
+//             wpnJobShort: "PLD",
+//             icon: "/icons/Manderville_Kite_Shield_Icon.png",
+//             isSelected: false
+//         },
+//         {
+//             id: 2,
+//             wpnName: "Manderville Axe",
+//             wpnJob: "Warrior",
+//             wpnJobShort: "WAR",
+//             icon: "/icons/Manderville_Axe_Icon.png",
+//             isSelected: false
+//         },
+//         {
+//             id: 3,
+//             wpnName: "Manderville Zweihander",
+//             wpnJob: "Dark Knight",
+//             wpnJobShort: "DRK",
+//             icon: "/icons/Manderville_Zweihander_Icon.png",
+//             isSelected: false
+//         },
+//         {
+//             id: 4,
+//             wpnName: "Manderville Gunblade",
+//             wpnJob: "Gunbreaker",
+//             wpnJobShort: "GNB",
+//             icon: "/icons/Manderville_Gunblade_Icon.png",
+//             isSelected: false
+//         },
+//         {
+//             id: 5,
+//             wpnName: "Manderville Spear",
+//             wpnJob: "Dragoon",
+//             wpnJobShort: "DRG",
+//             icon: "/icons/Manderville_Spear_Icon.png",
+//             isSelected: false
+//         },
+//         {
+//             id: 6,
+//             wpnName: "Manderville Scythe",
+//             wpnJob: "Reaper",
+//             wpnJobShort: "RPR",
+//             icon: "/icons/Manderville_Scythe_Icon.png",
+//             isSelected: false
+//         },
+//         {
+//             id: 7,
+//             wpnName: "Manderville Knuckles",
+//             wpnJob: "Monk",
+//             wpnJobShort: "MNK",
+//             icon: "/icons/Manderville_Knuckles_Icon.png",
+//             isSelected: false
+//         },
+//         {
+//             id: 8,
+//             wpnName: "Manderville Samurai Blade",
+//             wpnJob: "Samurai",
+//             wpnJobShort: "SAM",
+//             icon: "/icons/Manderville_Samurai_Blade_Icon.png",
+//             isSelected: false
+//         },
+//         {
+//             id: 9,
+//             wpnName: "Manderville Knives",
+//             wpnJob: "Ninja",
+//             wpnJobShort: "NIN",
+//             icon: "/icons/Manderville_Knives_Icon.png",
+//             isSelected: false
+//         },
+//         {
+//             id: 10,
+//             wpnName: "Manderville Harp Bow",
+//             wpnJob: "Bard",
+//             wpnJobShort: "BRD",
+//             icon: "/icons/Manderville_Harp_Bow_Icon.png",
+//             isSelected: false
+//         },
+//         {
+//             id: 11,
+//             wpnName: "Manderville Revolver",
+//             wpnJob: "Machinist",
+//             wpnJobShort: "MCH",
+//             icon: "/icons/Manderville_Revolver_Icon.png",
+//             isSelected: false
+//         },
+//         {
+//             id: 12,
+//             wpnName: "Manderville Chakrams",
+//             wpnJob: "Dancer",
+//             wpnJobShort: "DNC",
+//             icon: "/icons/Manderville_Chakrams_Icon.png",
+//             isSelected: false
+//         },
+//         {
+//             id: 13,
+//             wpnName: "Manderville Rod",
+//             wpnJob: "Black Mage",
+//             wpnJobShort: "BLM",
+//             icon: "/icons/Manderville_Rod_Icon.png",
+//             isSelected: false
+//         },
+//         {
+//             id: 14,
+//             wpnName: "Manderville Index",
+//             wpnJob: "Summoner",
+//             wpnJobShort: "SMN",
+//             icon: "/icons/Manderville_Index_Icon.png",
+//             isSelected: false
+//         },
+//         {
+//             id: 15,
+//             wpnName: "Manderville Rapier",
+//             wpnJob: "Red Mage",
+//             wpnJobShort: "RDM",
+//             icon: "/icons/Manderville_Rapier_Icon.png",
+//             isSelected: false
+//         },
+//         {
+//             id: 16,
+//             wpnName: "Manderville Cane",
+//             wpnJob: "White Mage",
+//             wpnJobShort: "WHM",
+//             icon: "/icons/Manderville_Cane_Icon.png",
+//             isSelected: false
+//         },
+//         {
+//             id: 17,
+//             wpnName: "Manderville Codex",
+//             wpnJob: "Scholar",
+//             wpnJobShort: "SCH",
+//             icon: "/icons/Manderville_Codex_Icon.png",
+//             isSelected: false
+//         },
+//         {
+//             id: 18,
+//             wpnName: "Manderville Torquetum",
+//             wpnJob: "Astrologian",
+//             wpnJobShort: "AST",
+//             icon: "/icons/Manderville_Torquetum_Icon.png",
+//             isSelected: false
+//         },
+//         {
+//             id: 19,
+//             wpnName: "Manderville Milpreves",
+//             wpnJob: "Sage",
+//             wpnJobShort: "SGE",
+//             icon: "/icons/Manderville_Milpreves_Icon.png",
+//             isSelected: false
+//         },
+//     ],
+//     [
+//         {
+//             id: 0,
+//             wpnName: "Amazing Manderville Sword",
+//             wpnJob: "Paladin",
+//             wpnJobShort: "PLD",
+//             icon: "/icons/Amazing_Manderville_Sword_Icon.png",
+//             isSelected: false
+//         },
+//         {
+//             id: 1,
+//             wpnName: "Amazing Manderville Kite Shield",
+//             wpnJob: "Paladin",
+//             wpnJobShort: "PLD",
+//             icon: "/icons/Amazing_Manderville_Kite_Shield_Icon.png",
+//             isSelected: false
+//         },
+//         {
+//             id: 2,
+//             wpnName: "Amazing Manderville Axe",
+//             wpnJob: "Warrior",
+//             wpnJobShort: "WAR",
+//             icon: "/icons/Amazing_Manderville_Axe_Icon.png",
+//             isSelected: false
+//         },
+//         {
+//             id: 3,
+//             wpnName: "Amazing Manderville Zweihander",
+//             wpnJob: "Dark Knight",
+//             wpnJobShort: "DRK",
+//             icon: "/icons/Amazing_Manderville_Zweihander_Icon.png",
+//             isSelected: false
+//         },
+//         {
+//             id: 4,
+//             wpnName: "Amazing Manderville Gunblade",
+//             wpnJob: "Gunbreaker",
+//             wpnJobShort: "GNB",
+//             icon: "/icons/Amazing_Manderville_Gunblade_Icon.png",
+//             isSelected: false
+//         },
+//         {
+//             id: 5,
+//             wpnName: "Amazing Manderville Spear",
+//             wpnJob: "Dragoon",
+//             wpnJobShort: "DRG",
+//             icon: "/icons/Amazing_Manderville_Spear_Icon.png",
+//             isSelected: false
+//         },
+//         {
+//             id: 6,
+//             wpnName: "Amazing Manderville Scythe",
+//             wpnJob: "Reaper",
+//             wpnJobShort: "RPR",
+//             icon: "/icons/Amazing_Manderville_Scythe_Icon.png",
+//             isSelected: false
+//         },
+//         {
+//             id: 7,
+//             wpnName: "Amazing Manderville Knuckles",
+//             wpnJob: "Monk",
+//             wpnJobShort: "MNK",
+//             icon: "/icons/Amazing_Manderville_Knuckles_Icon.png",
+//             isSelected: false
+//         },
+//         {
+//             id: 8,
+//             wpnName: "Amazing Manderville Samurai Blade",
+//             wpnJob: "Samurai",
+//             wpnJobShort: "SAM",
+//             icon: "/icons/Amazing_Manderville_Samurai_Blade_Icon.png",
+//             isSelected: false
+//         },
+//         {
+//             id: 9,
+//             wpnName: "Amazing Manderville Knives",
+//             wpnJob: "Ninja",
+//             wpnJobShort: "NIN",
+//             icon: "/icons/Amazing_Manderville_Knives_Icon.png",
+//             isSelected: false
+//         },
+//         {
+//             id: 10,
+//             wpnName: "Amazing Manderville Harp Bow",
+//             wpnJob: "Bard",
+//             wpnJobShort: "BRD",
+//             icon: "/icons/Amazing_Manderville_Harp_Bow_Icon.png",
+//             isSelected: false
+//         },
+//         {
+//             id: 11,
+//             wpnName: "Amazing Manderville Revolver",
+//             wpnJob: "Machinist",
+//             wpnJobShort: "MCH",
+//             icon: "/icons/Amazing_Manderville_Revolver_Icon.png",
+//             isSelected: false
+//         },
+//         {
+//             id: 12,
+//             wpnName: "Amazing Manderville Chakrams",
+//             wpnJob: "Dancer",
+//             wpnJobShort: "DNC",
+//             icon: "/icons/Amazing_Manderville_Chakrams_Icon.png",
+//             isSelected: false
+//         },
+//         {
+//             id: 13,
+//             wpnName: "Amazing Manderville Rod",
+//             wpnJob: "Black Mage",
+//             wpnJobShort: "BLM",
+//             icon: "/icons/Amazing_Manderville_Rod_Icon.png",
+//             isSelected: false
+//         },
+//         {
+//             id: 14,
+//             wpnName: "Amazing Manderville Index",
+//             wpnJob: "Summoner",
+//             wpnJobShort: "SMN",
+//             icon: "/icons/Amazing_Manderville_Index_Icon.png",
+//             isSelected: false
+//         },
+//         {
+//             id: 15,
+//             wpnName: "Amazing Manderville Rapier",
+//             wpnJob: "Red Mage",
+//             wpnJobShort: "RDM",
+//             icon: "/icons/Amazing_Manderville_Rapier_Icon.png",
+//             isSelected: false
+//         },
+//         {
+//             id: 16,
+//             wpnName: "Amazing Manderville Cane",
+//             wpnJob: "White Mage",
+//             wpnJobShort: "WHM",
+//             icon: "/icons/Amazing_Manderville_Cane_Icon.png",
+//             isSelected: false
+//         },
+//         {
+//             id: 17,
+//             wpnName: "Amazing Manderville Codex",
+//             wpnJob: "Scholar",
+//             wpnJobShort: "SCH",
+//             icon: "/icons/Amazing_Manderville_Codex_Icon.png",
+//             isSelected: false
+//         },
+//         {
+//             id: 18,
+//             wpnName: "Amazing Manderville Torquetum",
+//             wpnJob: "Astrologian",
+//             wpnJobShort: "AST",
+//             icon: "/icons/Amazing_Manderville_Torquetum_Icon.png",
+//             isSelected: false
+//         },
+//         {
+//             id: 19,
+//             wpnName: "Amazing Manderville Milpreves",
+//             wpnJob: "Sage",
+//             wpnJobShort: "SGE",
+//             icon: "/icons/Amazing_Manderville_Milpreves_Icon.png",
+//             isSelected: false
+//         },
+//     ],
+// ]
 
 export default data;
