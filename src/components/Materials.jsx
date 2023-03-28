5c9ede703e9b796d70eab6fea695ada9dc2a3af9
jacopo.congia <jacopo.congia@gmail.com>
2023-03-28 14:01:24 +0200

    Jacopocongia/select weapon refactoring (#5)
    * selectWeapon Refactoring
    
    * Refactored selectWeapon
    
    * Materials Refactoring

--- src/components/Materials.jsx
+++ src/components/Materials.jsx
@@ -1,25 +1,21 @@
-import React from 'react';
-
-function Materials({ tomestones, materials, materialName, icon }) {
-    return (
-        <div className='materials-container'>
-            <p className='astronomy'>
-            <img 
-                className='astronomy-icon'
-                alt={tomestones[0].name}
-                src={tomestones[0].icon}
-            />
-            {tomestones[0].name}: {materials / 3 * 1500}
-            </p>
-            <p className='primary-materials'>
+function Materials({ materials, weaponsTruths }) {
+    const materialsElements = materials.map(material => {
+        return (
+            <p key={material.id} className='primary-materials'>
                 <img 
                     className='material-icon'
-                    alt={materialName}
-                    src={icon}
+                    alt={material.name}
+                    src={material.icon}
                 />
-            {materialName}: {materials}
+                {material.name}: {material.reqPerWeapon * weaponsTruths.length}
             </p>
-          </div>
+        )
+    })
+    
+    return (
+        <div>
+            {materialsElements}
+        </div>
     )
 }
 
